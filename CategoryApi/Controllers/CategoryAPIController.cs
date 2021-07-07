9a4a93a79d6f3e0c8aee60024f5f857963c614ed
MeharshGoh <Meharsh.Gohadkar@citiustech.com>
2021-07-07 15:14:55 +0530

    ingress

---
+++ CategoryApi/Controllers/CategoryAPIController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CategoryApi.Models;
+
+namespace CategoryApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryAPIController : ControllerBase
+    {
+        CategoriesDb cats;
+
+        public CategoryAPIController()
+        {
+            cats = new CategoriesDb();
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(cats);
+        }
+    }
+}
